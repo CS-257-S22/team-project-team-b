4776bb98ed8f98a4b57488540e59f31f6c74aff8
Cole Kleinhans <34384401+Clean-Hands@users.noreply.github.com>
2022-04-11 09:35:30 -0500

    added testing boilerplate code

---
+++ test_predictor_of_passing.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestSOMETHING(unittest.TestCase):
+    def test_A_GOOD_NAME(self):
+        """ A GOOD DOCSTRING """
+        #make a bit of dummy data
+        #call your non-existent function
+        #check if the result of the call is what it should be
+        #   probably using self.assertEqual(something, something_else)
+
+if __name__ == '__main__':
+    unittest.main()
