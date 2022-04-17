07bd2558caeba0966cb5f4efb2587ce1c5e6f39e
Kai Ronfeldt Weiner <kairweiner@Kais-MacBook-Pro.local>
2022-04-17 16:48:34 -0500

    making sure everything is saving correctly, small updates before I make a lot of changes

--- deaths_per.py
+++ deaths_per.py
@@ -1,4 +1,14 @@
 #save this as a branch!!!!
 
-def deaths_per (age, state, gender, cause):
+class searchInfo:
+   def  __init__(age, state, gender,cause):
+       self.age = age
+       self.state = state
+       self.gender = gender
+       self.cause = cause
+
+
+def deaths_per (search):
     return 5
+
+#plan:
--- test_predictor_of_passing.py
+++ test_predictor_of_passing.py
@@ -7,6 +7,17 @@ class TestSOMETHING(unittest.TestCase):
         #call your non-existent function
         #check if the result of the call is what it should be
         #   probably using self.assertEqual(something, something_else)
+        data = search()
+        result = deaths_per(data)
+        self.assertEqual(result, 6)
+
+    def test_A_GOOD_NAME(self):
+        """ A GOOD DOCSTRING """
+        #make a bit of dummy data
+        #call your non-existent function
+        #check if the result of the call is what it should be
+        #   probably using self.assertEqual(something, something_else)
+
 
 if __name__ == '__main__':
     unittest.main()
