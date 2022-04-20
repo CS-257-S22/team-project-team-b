96646dadf4192f7a0b60e22c04b349eb8db748f2
nguyent4 <92402491+nguyent4@users.noreply.github.com>
2022-04-20 12:26:54 -0500

    Update leading_cause.py

--- leading_cause.py
+++ leading_cause.py
@@ -1,9 +1,33 @@
 """
 Written by Tin Nguyen
 """
-def find_leading_cause_based_on_state(input_state):
-  """ Do I need to write a caption? I do not know what is needed that is not explained by the title """
+
+def initialize_dataset(input_state,input_age,input_gender): 
+  """Function opens data set and calls function that determinds what the user input"""
   data_file = open("data.csv","r")
+  initialize_search(data_file,input_state,input_age,input_gender)
+  data_file.close()
+
+def initialize_search(data_file, state, age,gender): 
+  "Function determinds what varibles the the user input to determind how the data base should be searched"
+  if state != "" and age != "" and gender != "": 
+    find_leading_cause_based_on_stateAgeGender(data_file, state,age,gender) 
+  elif age != "" and state != "": 
+    find_leading_cause_based_on_stateAge(data_file, state,age) 
+  elif age != "" and gender !="": 
+    find_leading_cause_based_on_ageGender(data_file,age,gender)
+  elif state != "" and gender != "": 
+    find_leading_cause_based_on_stateGender(data_file,state,gender)
+  elif age != "": 
+    find_leading_cause_based_on_age(data_file, age) 
+  elif state != "":
+    find_leading_cause_based_on_state(data_file,state)
+  elif gender != "": 
+    find_leading_cause_based_on_gender(data_file,gender)
+
+
+def find_leading_cause_based_on_state(data_file, input_state):
+  """ Search  database for a specific state and create array for each instance """
   data = []
   for datapoint in data_file:
     if datapoint.split(",")[0] == input_state:
@@ -13,11 +37,10 @@ def find_leading_cause_based_on_state(input_state):
       instance = [state,cause_of_death,num_of_death]
       print(instance)
       data.append(instance)
-  data_file.close()
   return data
 
-def find_leading_cause_based_on_age(input_age): 
-  data_file = open("data.csv","r")
+def find_leading_cause_based_on_age(data_file,input_age): 
+  """ Search  database for a specific age and create array for each instance """
   data = []
   for datapoint in data_file:
     if datapoint.split(",")[1] == input_age:
@@ -26,12 +49,11 @@ def find_leading_cause_based_on_age(input_age):
       num_of_death =  datapoint.split(",")[5]
       instance = [age,cause_of_death,num_of_death]
       data.append(instance)
-  data_file.close()
   return data
 
   
-def find_leading_cause_based_on_gender(input_gender): 
-  data_file = open("data.csv","r")
+def find_leading_cause_based_on_gender(data_file,input_gender): 
+  """ Search  database for a specific gender and create array for each instance """
   data = []
   for datapoint in data_file:
     if datapoint.split(",")[2] == input_gender:
@@ -40,12 +62,11 @@ def find_leading_cause_based_on_gender(input_gender):
       num_of_death =  datapoint.split(",")[5]
       instance = [gender,cause_of_death,num_of_death]
       data.append(instance)
-  data_file.close()
   return data
 
 
-def find_leading_cause_based_on_ageState(input_state,input_age): 
-  data_file = open("data.csv","r")
+def find_leading_cause_based_on_stateAge(data_file, input_state,input_age):
+  """ Search  database for a specific state and age and create array for each instance """
   data = []
   for datapoint in data_file:
     """should I put the varibles out here or in the if loop"""
@@ -56,11 +77,10 @@ def find_leading_cause_based_on_ageState(input_state,input_age):
       num_of_death =  datapoint.split(",")[5]
       instance = [state, age,cause_of_death,num_of_death]
       data.append(instance) 
-  data_file.close()
   return data
 
-def find_leading_cause_based_on_stateGender(input_state,input_gender):
-  data_file = open("data.csv","r")
+def find_leading_cause_based_on_stateGender(data_file,input_state,input_gender):
+  """ Search  database for a specific state and gender and create array for each instance """
   data = []
   for datapoint in data_file:
     """should I put the varibles out here or in the if loop"""
@@ -71,11 +91,10 @@ def find_leading_cause_based_on_stateGender(input_state,input_gender):
       num_of_death =  datapoint.split(",")[5]
       instance = [state,gender,cause_of_death,num_of_death]
       data.append(instance) 
-  data_file.close()
   return data
 
-def find_leading_cause_based_on_ageGender(input_age,input_gender): 
-  data_file = open("data.csv","r")
+def find_leading_cause_based_on_ageGender(data_file, input_age,input_gender): 
+    """ Search  database for a specific age and gender and create array for each instance """
   data = []
   for datapoint in data_file:
     """should I put the varibles out here or in the if loop"""
@@ -87,11 +106,10 @@ def find_leading_cause_based_on_ageGender(input_age,input_gender):
       instance = [age,gender,cause_of_death,num_of_death]
       data.append(instance) 
       print(instance)
-  data_file.close()
   return data
   
-def find_leading_cause_based_on_stateAgeGender(input_state,input_age,input_gender): 
-  data_file = open("data.csv","r")
+def find_leading_cause_based_on_stateAgeGender(data_file,input_state,input_age,input_gender):
+    """ Search  database for a specific state, age and gender and create array for each instance """
   data = []
   for datapoint in data_file:
     """should I put the varibles out here or in the if loop"""
@@ -104,12 +122,4 @@ def find_leading_cause_based_on_stateAgeGender(input_state,input_age,input_gende
       instance = [state,age,gender,cause_of_death,num_of_death]
       data.append(instance)
       print(instance)
-  data_file.close()
   return data
-
-
-def main():
-   find_leading_cause_based_on_stateAgeGender('"New York"',"49",'"F"')
-
-if __name__ == "__main__": 
-   main() 
