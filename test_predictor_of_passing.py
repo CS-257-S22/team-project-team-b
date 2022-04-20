67a10c2f02bcc470afceac9bcadaf45ca26ecda8
Kai Ronfeldt Weiner <kairweiner@Kais-MacBook-Pro.local>
2022-04-20 00:06:36 -0500

    fixed syntax error in test_predictor_of_passing

--- test_predictor_of_passing.py
+++ test_predictor_of_passing.py
@@ -5,55 +5,55 @@ class TestSOMETHING(unittest.TestCase):
 
     def test_purposely_wrong_return(self):
         """ Test that it recognizes the wrong answer """
-        search = searchInfo("California",None,None,None)
+        search = SearchInfo("California",None,None,None)
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 100)
 
     def test_sum_deaths_by_state(self):
         """ Test that it can sum the data for one state """
-        search = searchInfo("California",None,None,None)
+        search = SearchInfo("California",None,None,None)
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 90)
     
     def test_sum_deaths_by_age(self):
         """ Test that it can sum the data for one age """
-        search = searchInfo(None,"37",None,None)
+        search = SearchInfo(None,"37",None,None)
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 100)
     
     def test_sum_deaths_by_gender(self):
         """ Test that it can sum the data for one gender """
-        search = searchInfo(None,None,"M",None)
+        search = SearchInfo(None,None,"M",None)
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 130)
 
     def test_sum_deaths_by_cause(self):
         """ Test that it can sum the data for one state """
-        search = searchInfo(None,None,None,"Infection")
+        search = SearchInfo(None,None,None,"Infection")
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 70)
 
     def test_intersection_sum_deaths_by_age_and_gender(self):
         """ Test that it can sum the data for the intersection of age and gender """
-        search = searchInfo(None,"49","F",None)
+        search = SearchInfo(None,"49","F",None)
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 70)
     
     def test_intersection_single_deaths_by_age_and_gender(self):
         """ Test that it can find the data for the intersection of age and gender """
-        search = searchInfo(None,"37","F",None)
+        search = SearchInfo(None,"37","F",None)
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 20)
 
     def test_get_single_datapoint_all_terms(self):
         """ Test that it can sum the data for one search with all terms specified """
-        search = searchInfo("Nebraska","2","F","Infection")
+        search = SearchInfo("Nebraska","2","F","Infection")
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 20)
     
     def test_get_all_deaths(self):
         """ Test that it can sum of all deaths """
-        search = searchInfo(None,None,None,None)
+        search = SearchInfo(None,None,None,None)
         result = deaths_per(search, deaths_data)
         self.assertEqual(result, 270)
 
