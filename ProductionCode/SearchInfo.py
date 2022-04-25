3c54cf56e513289f4a25302bbba1bdc984d84b25
Kai Ronfeldt Weiner <kairweiner@Kais-MacBook-Pro.local>
2022-04-25 15:45:57 -0500

    fixed annotation style for deaths_per files

--- ProductionCode/SearchInfo.py
+++ ProductionCode/SearchInfo.py
@@ -7,53 +7,67 @@ Stores information about a search for the amount of deaths given a set of search
 """
 class SearchInfo:
 
-    """
-    Sets up an instance of SearchInfo.
-    @param state : state being searched for. If this term is not being searched: None
-    @param age : age being searched for. If this term is not being searched: None
-    @param gender : gender being searched for. If this term is not being searched: None
-    @param cause : cause of death being searched for. If this term is not being searched: None
-    """
     def __init__(self, state, age, gender, cause):
-       self.state = state
-       self.age = age
-       self.gender = gender
-       self.cause = cause
+        """
+        Sets up an instance of SearchInfo.
+
+        Args
+            state : state being searched for. If this term is not being searched: None
+            age : age being searched for. If this term is not being searched: None
+            gender : gender being searched for. If this term is not being searched: None
+            cause : cause of death being searched for. If this term is not being searched: None
+        Return:
+            An SearchInfo object.
+        """
+        self.state = state
+        self.age = age
+        self.gender = gender
+        self.cause = cause
     
-    """
-    Sets the state being searched for to a specified input.
-    @param new_state : the new state being searched for
-    """
     def set_state(self, new_state):
+        """
+        Sets the state being searched for to a specified input.
+
+        Args:
+            new_state : the new state being searched for
+        """
         self.state = new_state
 
-    """
-    Sets the age being searched for to a specified input.
-    @param new_age : the new age being searched for
-    """
     def set_age(self, new_age):
+        """
+        Sets the age being searched for to a specified input.
+
+        Args:
+            new_age : the new age being searched for
+        """
         self.age = new_age
 
-    """
-    Sets the gender being searched for to a specified input.
-    @param new_gender : the new gender being searched for
-    """
     def set_gender(self, new_gender):
+        """
+        Sets the gender being searched for to a specified input.
+        Args:
+            new_gender : the new gender being searched for
+        """
         self.gender = new_gender
 
-    """
-    Sets the cause of death being searched for to a specified input.
-    @param new_cause : the new cause of death being searched for
-    """
     def set_cause(self, new_cause):
+        """
+        Sets the cause of death being searched for to a specified input.
+
+        Args:
+            new_cause : the new cause of death being searched for
+        """
         self.cause = new_cause
     
-    """
-    Returns a search term specified by a string.
-    @param term : the term being requested
-    @return : the value of the term being requested or an error message if the term does not exist
-    """
     def get_term_from_string(self, term):
+        """
+        Returns a search term specified by a string.
+
+        Args:
+            term : the term being requested
+        Return
+            the value of the term being requested or an error message if the term does not exist
+        """
         if(term == "state"):
             return self.state
         if(term == "age"):
