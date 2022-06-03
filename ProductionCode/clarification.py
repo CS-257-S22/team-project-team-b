4db9cccfbe5efe0efabeff14c90373d9c13e7143
Jonas Bartels <bartelsj@carleton.edu>
2022-06-03 15:56:55 -0500

    clarification2

--- ProductionCode/clarification.py
+++ ProductionCode/clarification.py
@@ -2,17 +2,21 @@
 For very common words, such as malignant neoplasms, add paranthetical explination
 """
 def clarify(cause):
-    keywords = ["malignant neoplasms"]
-    additions = ["cancer"]
+    keywords = ["- Malignant neoplasms", "Malignant neoplasm "]
+    additions = ["cancer", "cancer"]
     i = 0
     for keyword in keywords:
         if keyword in cause:
             cause = cause.split(keyword)
-            addition = f" ({additions[i]})"
+            addition = f"({additions[i]}) "
             cause = cause[0]+keyword+addition+cause[1]
         i += 1
     return cause
 
+if __name__ == "__main__":
+    list1 = ["Oesophagus, unspecified - Malignant neoplasms", "Malignant neoplasm of prostate"]
+    for line in list1:
+        print(clarify(line))
 
 
 
